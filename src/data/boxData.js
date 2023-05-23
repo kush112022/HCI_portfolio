55336e12e94581a9572c1f350f493163cd7414d8
rauldotgit <mail@raul.codes>
2023-05-23 20:09:16 +0200

    Changed image directory for vercel

--- src/data/boxData.js
+++ src/data/boxData.js
@@ -2,28 +2,28 @@ const Data = [
     {   
         id: 0,
         alt: 'Project Image 1',
-        img: './src/assets/grain1.jpg',    
+        img: 'public/grain1.jpg',    
         title: 'React Space',
         text: 'Handy tool belt to create amazing AR components in a React app, with redux integration view middleware.'    
     },
     {   
         id: 1,
         alt: 'Project Image 2',
-        img: './src/assets/grain2.jpg',    
+        img: 'public/grain2.jpg',    
         title: 'React Infinite Scroll',
         text: 'A scrollable bottom sheet with virtualisation support, native animations at 60 FPS and fully implemented in JS land.'    
     },
     {   
         id: 2,
         alt: 'Project Image 3',
-        img: './src/assets/grain3.jpg',    
+        img: 'public/grain3.jpg',    
         title: 'Photo Gallery',
         text: 'A One-stop shop for photographers to share and monetize their photos, allowing them to have a second source of income.'    
     },
     {   
         id: 3,
         alt: 'Project Image 4',
-        img: './src/assets/grain4.jpg',    
+        img: 'public/grain4.jpg',    
         title: 'Event Planner',
         text: 'A mobile application for leisure seekers to discover unique events and activities in their city with a few taps.'    
     }
